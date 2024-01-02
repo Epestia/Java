b66859ab9369e30cee90d15c9dc5d855537dbeae
Giovanni Dylan Menegon <64831113+Epestia@users.noreply.github.com>
2024-01-02 19:51:14 +0100

    newversion

--- 02-Addition/Addition.java
+++ 02-Addition/Addition.java
@@ -10,21 +10,21 @@ public class Addition {
         // Créer un objet Scanner pour lire l'entrée de l'utilisateur
         Scanner scanner = new Scanner(System.in);
 
-        // Demander à l'utilisateur de saisir le premier nombre
+        // Demander à l'utilisateur de saisir dans la console le premier nombre :
         System.out.print("Entrez le premier nombre : ");
         double premierNombre = scanner.nextDouble();
 
-        // Demander à l'utilisateur de saisir le deuxième nombre
-        System.out.print("Entrez le deuxième nombre : ");
+        // Demander à l'utilisateur de saisir dans la console le deuxième nombre :
+        System.out.print("Entrez le deuxième nombre : "); 
         double deuxiemeNombre = scanner.nextDouble();
 
-        // Calculer la somme des deux nombres
+        // Calculer la somme des deux nombres :
         double somme = premierNombre + deuxiemeNombre;
 
-        // Afficher le résultat
+        // Afficher le résultat :
         System.out.println("La somme de " + premierNombre + " et " + deuxiemeNombre + " est : " + somme);
 
-        // Fermer le scanner
+        // Fermer le scanner :
         scanner.close();
     }
 }
